4bd82faac275a867776afecb0348670569b2e20a
Krzysztof Sobczak <k.sobczak@emlbox.com>
2019-03-01 19:30:58 +0000

    db created

--- db_create.sql
+++ db_create.sql
@@ -3,10 +3,42 @@
 CREATE DATABASE IF NOT EXISTS cpv;
 
 CREATE USER IF NOT EXISTS 'cpv'@'localhost';
-ALTER USER 'cpv'@'localhost'IDENTIFIED BY ; --needs password added;
-GRANT SELECT, INSERT, DELETE ON cpv.* TO 'cpv'@'localhost';
+ALTER USER 'cpv'@'localhost'IDENTIFIED BY ; --pass in keepass;
+GRANT SELECT, INSERT, UPDATE, DELETE ON cpv.* TO 'cpv'@'localhost';
 FLUSH PRIVILEGES;
 
 -- if running on docker connect with hostname and port
 -- mysql -h localhost -P 3306 --protocol=tcp -u cpv -p
+
+CREATE TABLE IF NOT EXISTS `cpv`.`params_main` (
+  `emi_master` VARCHAR(8) NOT NULL,
+  `parameter` VARCHAR(8) NOT NULL,
+  `family` VARCHAR(12) NOT NULL,
+  `area` VARCHAR(12) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `dataformat` VARCHAR(12) NULL,
+  PRIMARY KEY (`emi_master`, `parameter`));
+
+CREATE TABLE IF NOT EXISTS `cpv`.`params_taggers` (
+  `emi_master` VARCHAR(8) NOT NULL,
+  `emi_parent` VARCHAR(8) NOT NULL,
+  `emi_sub` VARCHAR(8) NOT NULL,
+  `parameter` VARCHAR(8) NOT NULL,
+  `area` VARCHAR(12) NOT NULL,
+  `family` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`emi_master`, `emi_parent`, `emi_sub`, `parameter`));
+
+CREATE TABLE IF NOT EXISTS `cpv`.`params_aggregate` (
+  `emi_master` VARCHAR(8) NOT NULL,
+  `emi_parent` VARCHAR(8) NOT NULL,
+  `emi_sub` VARCHAR(8) NOT NULL,
+  `parameter` VARCHAR(8) NOT NULL,
+  `groupid` INT NOT NULL,
+  `family` VARCHAR(12) NOT NULL,
+  `area` VARCHAR(12) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
+  `dataformat` VARCHAR(12) NULL,
+  `function` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`emi_master`, `emi_parent`, `emi_sub`, `parameter`));
+
 
